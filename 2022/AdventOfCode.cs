61d7ebf5e76ddc3040d4ec211b48b79d9e519b3f
Albond87 <Albond8746@gmail.com>
2023-12-01 00:39:50 +0000

    Day 7

--- 2022/AdventOfCode.cs
+++ 2022/AdventOfCode.cs
@@ -2,9 +2,9 @@
 {
     static void Main()
     {
-        string input = System.IO.File.ReadAllText(@"Inputs/input06.txt");
-        //string[] input = System.IO.File.ReadAllLines(@"Inputs/input06.txt");
-        Console.WriteLine(Puzzle06.Part1(input));
-        Console.WriteLine(Puzzle06.Part2(input));
+        //string input = System.IO.File.ReadAllText(@"Inputs/input06.txt");
+        string[] input = System.IO.File.ReadAllLines(@"Inputs/input07.txt");
+        Console.WriteLine(Puzzle07.Part1(input));
+        Console.WriteLine(Puzzle07.Part2(input));
     }
 }
---
+++ 2022/Puzzle07.cs
@@ -0,0 +1,101 @@
+public static class Puzzle07
+{
+    public static int Part1(string[] lines)
+    {
+        string currentDir = "/";
+        List<string> dirs = new List<string>{"/"};
+        int dirNameSize = 0;
+        Dictionary<string, int> dirSizes = new System.Collections.Generic.Dictionary<string, int>();
+        dirSizes.Add("/",0);
+        for (int i=0; i<lines.Length; i++) {
+            Console.WriteLine(lines[i]);
+            if (lines[i].StartsWith("$ cd")) {
+                string dir = lines[i].Substring(5);
+                if (dir == "/") {
+                    currentDir = "/";
+                    dirs = new List<string>{"/"};
+                }
+                else if (dir == "..") {
+                    dirs.RemoveAt(dirs.Count - 1);
+                    currentDir = dirs.ElementAt(dirs.Count - 1);//currentDir.Substring(0,currentDir.Length-dirNameSize-1);
+                }
+                else {
+                    dirNameSize = dir.Length;
+                    currentDir += dir + "/";
+                    dirs.Add(currentDir);
+                }
+            } 
+            else if (lines[i] == "$ ls") {
+                i += 1;
+                while (!lines[i].StartsWith("$")) {
+                    if (lines[i].StartsWith("dir")) dirSizes.Add(currentDir + lines[i].Substring(4) + "/", 0);
+                    else {
+                        foreach (string d in dirs)
+                            dirSizes[d] += int.Parse(lines[i].Split(" ")[0]);
+                    }
+                    i += 1;
+                    if (i == lines.Length) break;
+                }
+                i -=1 ;
+            }
+            //Console.WriteLine(currentDir);
+        }
+        int total = 0;
+        foreach (string k in dirSizes.Keys) {
+            //Console.WriteLine(k + ": " + dirSizes[k]);
+            int size = dirSizes[k];
+            if (size <= 100000) total += size;
+        }            
+        return total;
+    }
+
+    public static int Part2(string[] lines)
+    {
+        string currentDir = "/";
+        List<string> dirs = new List<string>{"/"};
+        int dirNameSize = 0;
+        Dictionary<string, int> dirSizes = new System.Collections.Generic.Dictionary<string, int>();
+        dirSizes.Add("/",0);
+        for (int i=0; i<lines.Length; i++) {
+            Console.WriteLine(lines[i]);
+            if (lines[i].StartsWith("$ cd")) {
+                string dir = lines[i].Substring(5);
+                if (dir == "/") {
+                    currentDir = "/";
+                    dirs = new List<string>{"/"};
+                }
+                else if (dir == "..") {
+                    dirs.RemoveAt(dirs.Count - 1);
+                    currentDir = dirs.ElementAt(dirs.Count - 1);//currentDir.Substring(0,currentDir.Length-dirNameSize-1);
+                }
+                else {
+                    dirNameSize = dir.Length;
+                    currentDir += dir + "/";
+                    dirs.Add(currentDir);
+                }
+            } 
+            else if (lines[i] == "$ ls") {
+                i += 1;
+                while (!lines[i].StartsWith("$")) {
+                    if (lines[i].StartsWith("dir")) dirSizes.Add(currentDir + lines[i].Substring(4) + "/", 0);
+                    else {
+                        foreach (string d in dirs)
+                            dirSizes[d] += int.Parse(lines[i].Split(" ")[0]);
+                    }
+                    i += 1;
+                    if (i == lines.Length) break;
+                }
+                i -=1 ;
+            }
+            //Console.WriteLine(currentDir);
+        }
+        int target = 30000000-(70000000-dirSizes["/"]);
+        int smallest = dirSizes["/"];
+        foreach (string k in dirSizes.Keys) {
+            //Console.WriteLine(k + ": " + dirSizes[k]);
+            int size = dirSizes[k];
+            if (size > target && size < smallest) smallest = size;
+        }            
+        return smallest;
+    }
+}

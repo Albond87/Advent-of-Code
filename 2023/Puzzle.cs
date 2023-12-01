057af505875980d511f814ff87335bc7f155d69c
Albond87 <Albond8746@gmail.com>
2023-12-01 00:39:59 +0000

    Updated gitignore

--- 2023/Puzzle.cs
+++ 2023/Puzzle.cs
@@ -1,7 +1,7 @@
 public abstract class Puzzle(string day)
 {
-    protected string input = System.IO.File.ReadAllText(@"Inputs/input" + day + ".txt").Replace("\r","");
-    protected string[] inputs = System.IO.File.ReadAllLines(@"Inputs/input" + day + ".txt");
+    protected string input = File.ReadAllText(@"Inputs/input" + day + ".txt").Replace("\r","");
+    protected string[] inputs = File.ReadAllLines(@"Inputs/input" + day + ".txt");
 
     public abstract void Part1();
 

1848eeeb74b307809d093594599a28d8545e1ea8
Albond87 <Albond8746@gmail.com>
2023-12-04 12:17:03 +0000

    Day 4

--- 2023/Puzzle04.cs
+++ 2023/Puzzle04.cs
@@ -1,14 +1,36 @@
 public class Puzzle04 : Puzzle
 {
-    public Puzzle04() : base("04") { }
+    readonly IEnumerable<string[]> numbers;
+    readonly IEnumerable<HashSet<int>> winning;
+    readonly IEnumerable<HashSet<int>> owned;
+
+    public Puzzle04() : base("04") { 
+        numbers = inputs.Select(i => i.Split(": ")[1].Split(" | "));
+        winning = numbers.Select(i => new HashSet<int>(i[0].Split(" ").Where(n => n != "").Select(n => int.Parse(n.Trim()))));
+        owned = numbers.Select(i => new HashSet<int>(i[1].Split(" ").Where(n => n != "").Select(n => int.Parse(n.Trim()))));
+    }
 
     public override void Part1()
     {
-        Console.WriteLine();
+        double sum = 0;
+        for (int i=0; i<inputs.Length; i++) {
+            int wins = winning.ElementAt(i).Intersect(owned.ElementAt(i)).Count();
+            if (wins > 0) {
+                sum += Math.Pow(2,wins-1);
+            }
+        }
+        Console.WriteLine(sum);
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        int[] cardCounts = Enumerable.Repeat(1,inputs.Length).ToArray();
+        for (int i=0; i<inputs.Length; i++) {
+            int wins = winning.ElementAt(i).Intersect(owned.ElementAt(i)).Count();
+            for (int j=0; j<wins; j++) {
+                cardCounts[i+j+1] += cardCounts[i];
+            }
+        }
+        Console.WriteLine(cardCounts.Sum());
     }
 }

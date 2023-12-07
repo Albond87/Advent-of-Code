9d330cc780877f1733f7e63e38368430416e3eba
Albond87 <Albond8746@gmail.com>
2023-12-07 02:53:26 +0000

    Day 6

--- 2023/AdventOfCode.cs
+++ 2023/AdventOfCode.cs
@@ -2,7 +2,7 @@
 {
     static void Main()
     {
-        Puzzle puzzle = new Puzzle04();
+        Puzzle puzzle = new Puzzle06();
         puzzle.Part1();
         puzzle.Part2();
 
--- 2023/Puzzle06.cs
+++ 2023/Puzzle06.cs
@@ -4,11 +4,39 @@ public class Puzzle06 : Puzzle
 
     public override void Part1()
     {
-        Console.WriteLine();
+        int[] times = inputs[0].Split(" ").Where(i => i != " " && i != "").Skip(1).Select(i => int.Parse(i)).ToArray();
+        int[] distances = inputs[1].Split(" ").Where(i => i != " " && i != "").Skip(1).Select(i => int.Parse(i)).ToArray();
+        int product = 1;
+        for (int i = 0; i<times.Length; i++) {
+            int b = times[i];
+            int c = distances[i];
+            double sqrt = Math.Sqrt(b*b - (4*c));
+            double lower = (-b + sqrt) / -2;
+            if ((int)lower == lower) {
+                lower += 1;
+            }
+            double upper = (-b - sqrt) / -2;
+            if ((int)upper == upper) {
+                upper -= 1;
+            }
+            product *= (int)Math.Floor(upper)-(int)Math.Ceiling(lower)+1;
+        }
+        Console.WriteLine(product);
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        long b = long.Parse(inputs[0].Replace(" ","")[5..]);
+        long c = long.Parse(inputs[1].Replace(" ","")[9..]);
+        double sqrt = Math.Sqrt(b*b - (4*c));
+        double lower = (-b + sqrt) / -2;
+        if ((int)lower == lower) {
+            lower += 1;
+        }
+        double upper = (-b - sqrt) / -2;
+        if ((int)upper == upper) {
+            upper -= 1;
+        }
+        Console.WriteLine((int)Math.Floor(upper)-(int)Math.Ceiling(lower)+1);
     }
 }

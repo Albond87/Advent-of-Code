df5a2c18cd108cae745eaab5cf9961ce8170c4c1
Albond87 <Albond8746@gmail.com>
2024-12-03 19:31:19 +0000

    Improved 2024 day 3

--- 2024/Puzzle03.cs
+++ 2024/Puzzle03.cs
@@ -2,14 +2,11 @@ using System.Text.RegularExpressions;
 
 public class Puzzle03 : Puzzle
 {
-    readonly Regex regex;
-
-    public Puzzle03() : base("03") {
-        regex = new Regex(@"mul\([0-9]+,[0-9]+\)");
-    }
+    public Puzzle03() : base("03") { }
 
     public override void Part1()
     {
+        Regex regex = new Regex(@"mul\([0-9]+,[0-9]+\)");
         var ops = regex.Matches(input).ToArray();
         int sum = 0;
         foreach (var op in ops) {
@@ -21,24 +18,39 @@ public class Puzzle03 : Puzzle
 
     public override void Part2()
     {
-        string enabled = "";
-        int pos = 0;
-        while (pos > -1) {
-            int nextpos = input.IndexOf("don't()", pos);
-            if (nextpos > -1) {
-                enabled += input.Substring(pos, nextpos-pos);
-                pos = input.IndexOf("do()", nextpos);
-            }
-            else {
-                enabled += input.Substring(pos);
-                pos = -1;
-            }
-        }
-        var ops = regex.Matches(enabled).ToArray();
+        // Original solution, finding don't() and do() one at a time
+        // string enabled = "";
+        // int pos = 0;
+        // while (pos > -1) {
+        //     int nextpos = input.IndexOf("don't()", pos);
+        //     if (nextpos > -1) {
+        //         enabled += input.Substring(pos, nextpos-pos);
+        //         pos = input.IndexOf("do()", nextpos);
+        //     }
+        //     else {
+        //         enabled += input.Substring(pos);
+        //         pos = -1;
+        //     }
+        // }
+        // input = enabled;
+        // Part1();
+
+        // Improved solution, using a single combined regex
+        Regex regex = new Regex(@"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)");
+        var ops = regex.Matches(input).ToArray();
         int sum = 0;
+        bool enabled = true;
         foreach (var op in ops) {
-            var nums = op.Value[4..^1].Split(",").Select(int.Parse).ToArray();
-            sum += nums[0] * nums[1];
+            if (op.Value == "do()") {
+                enabled = true;
+            }
+            else if (op.Value == "don't()") {
+                enabled = false;
+            }
+            else if (enabled) {
+                var nums = op.Value[4..^1].Split(",").Select(int.Parse).ToArray();
+                sum += nums[0] * nums[1];    
+            }
         }
         Console.WriteLine(sum);
     }

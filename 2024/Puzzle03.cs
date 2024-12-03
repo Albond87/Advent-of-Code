91fdc249b2363b2adfdb2669a72522e5b439b972
Albond87 <Albond8746@gmail.com>
2024-12-03 18:52:51 +0000

    2024 day 3

--- 2024/Puzzle03.cs
+++ 2024/Puzzle03.cs
@@ -1,14 +1,45 @@
+using System.Text.RegularExpressions;
+
 public class Puzzle03 : Puzzle
 {
-    public Puzzle03() : base("03") { }
+    readonly Regex regex;
+
+    public Puzzle03() : base("03") {
+        regex = new Regex(@"mul\([0-9]+,[0-9]+\)");
+    }
 
     public override void Part1()
     {
-        Console.WriteLine();
+        var ops = regex.Matches(input).ToArray();
+        int sum = 0;
+        foreach (var op in ops) {
+            var nums = op.Value[4..^1].Split(",").Select(int.Parse).ToArray();
+            sum += nums[0] * nums[1];
+        }
+        Console.WriteLine(sum);
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        string enabled = "";
+        int pos = 0;
+        while (pos > -1) {
+            int nextpos = input.IndexOf("don't()", pos);
+            if (nextpos > -1) {
+                enabled += input.Substring(pos, nextpos-pos);
+                pos = input.IndexOf("do()", nextpos);
+            }
+            else {
+                enabled += input.Substring(pos);
+                pos = -1;
+            }
+        }
+        var ops = regex.Matches(enabled).ToArray();
+        int sum = 0;
+        foreach (var op in ops) {
+            var nums = op.Value[4..^1].Split(",").Select(int.Parse).ToArray();
+            sum += nums[0] * nums[1];
+        }
+        Console.WriteLine(sum);
     }
 }

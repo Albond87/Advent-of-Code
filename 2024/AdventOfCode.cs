0b7c417e60f5169def317ee6e0121a98d2fbb128
Albond87 <Albond8746@gmail.com>
2024-12-08 14:06:08 +0000

    2024 day 8

--- 2024/AdventOfCode.cs
+++ 2024/AdventOfCode.cs
@@ -2,7 +2,7 @@
 {
     static void Main()
     {
-        Puzzle puzzle = new Puzzle07();
+        Puzzle puzzle = new Puzzle08();
         puzzle.Part1();
         puzzle.Part2();
 
--- 2024/Puzzle08.cs
+++ 2024/Puzzle08.cs
@@ -2,13 +2,67 @@ public class Puzzle08 : Puzzle
 {
     public Puzzle08() : base("08") { }
 
+    int CountAntinodes(bool includeHarmonics=false) {
+        HashSet<char> frequencies = [];
+        HashSet<(int,int)> antinodes = [];
+        int height = inputs.Length;
+        int width = inputs[0].Length;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                char antenna = inputs[y][x];
+                if (antenna != '.' && !frequencies.Contains(antenna)) {
+                    for (int y2 = y; y2 < height; y2++) {
+                        int startx = y2 == y ? x + 1 : 0;
+                        for (int x2 = startx; x2 < width; x2++) {
+                            if (inputs[y2][x2] == antenna) {
+                                int xdiff = x2 - x;
+                                int ydiff = y2 - y;
+                                int x3, y3;
+                                if (!includeHarmonics) {
+                                    // Part 1
+                                    x3 = x2 + xdiff;
+                                    y3 = y2 + ydiff;
+                                    if (x3 >= 0 && x3 < width && y3 < height) {
+                                        antinodes.Add((x3,y3));
+                                    }
+                                    x3 = x - xdiff;
+                                    y3 = y - ydiff;
+                                    if (x3 >= 0 && x3 < width && y3 >= 0) {
+                                        antinodes.Add((x3,y3));
+                                    }
+                                    continue;
+                                }
+                                // Part 2
+                                x3 = x2;
+                                y3 = y2;
+                                while (x3 >= 0 && x3 < width && y3 < height) {
+                                    antinodes.Add((x3,y3));
+                                    x3 += xdiff;
+                                    y3 += ydiff;
+                                }
+                                x3 = x;
+                                y3 = y;
+                                while (x3 >= 0 && x3 < width && y3 >= 0) {
+                                    antinodes.Add((x3,y3));
+                                    x3 -= xdiff;
+                                    y3 -= ydiff;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return antinodes.Count;
+    }
+
     public override void Part1()
     {
-        Console.WriteLine();
+        Console.WriteLine(CountAntinodes());
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        Console.WriteLine(CountAntinodes(true));
     }
 }

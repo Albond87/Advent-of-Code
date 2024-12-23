2e21789df11dc5e4ba6447b3099444c708e76ea9
Albond87 <Albond8746@gmail.com>
2024-12-23 12:49:08 +0000

    2024 day 23

--- 2024/AdventOfCode.cs
+++ 2024/AdventOfCode.cs
@@ -2,7 +2,7 @@
 {
     static void Main()
     {
-        Puzzle puzzle = new Puzzle22();
+        Puzzle puzzle = new Puzzle23();
         puzzle.Part1();
         puzzle.Part2();
 
--- 2024/Puzzle23.cs
+++ 2024/Puzzle23.cs
@@ -1,14 +1,77 @@
 public class Puzzle23 : Puzzle
 {
-    public Puzzle23() : base("23") { }
+    HashSet<(string,string)> connections = [];
+
+    public Puzzle23() : base("23") { 
+        foreach (var input in inputs) {
+            connections.Add((input.Split("-")[0],input.Split("-")[1]));
+            connections.Add((input.Split("-")[1],input.Split("-")[0]));
+        }
+    }
 
     public override void Part1()
     {
-        Console.WriteLine();
+        HashSet<(string,string,string)> countedsets = [];
+        int count = 0;
+        foreach (var connection in connections) {
+            if (connection.Item1.StartsWith('t')) {
+                var setsof3 = connections.Where(c => c.Item1 == connection.Item2 && connections.Contains((c.Item2, connection.Item1)));
+                foreach (var set in setsof3) {
+                    string[] computers = new string[]{connection.Item1, set.Item1, set.Item2}.Order().ToArray();
+                    (string,string,string) computerstuple = (computers[0],computers[1],computers[2]);
+                    if (!countedsets.Contains(computerstuple)) {
+                        count++;
+                        countedsets.Add(computerstuple);
+                    }
+                }
+            }
+        }
+        Console.WriteLine(count);
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        // BFS the graph for sets that are fully connected
+        List<HashSet<string>> connectedsets = [];
+        HashSet<string> explored = [];
+        foreach (var connection in connections) {
+            if (explored.Contains(connection.Item1)) {
+                continue;
+            }
+            HashSet<string> currentset = [];
+            Queue<string> frontier = [];
+            frontier.Enqueue(connection.Item1);
+            while (frontier.Count > 0) {
+                string computer = frontier.Dequeue();
+
+                // Check that the computer is fully connected to everything else in the current set
+                bool allconnected = true;
+                foreach (var s in currentset) {
+                    if (!connections.Contains((computer,s))) {
+                        allconnected = false;
+                        break;
+                    }
+                }
+                if (!allconnected) {
+                    continue;
+                }
+
+                // Find everything that's connected to the current computer and add them to the frontier if not already explored
+                var connected = connections.Where(c => c.Item1 == computer);
+                foreach (var c in connected) {
+                    if (!currentset.Contains(c.Item2)) {
+                        frontier.Enqueue(c.Item2);
+                    }
+                }
+                currentset.Add(computer);
+                explored.Add(computer);
+            }
+            connectedsets.Add(currentset);
+        }
+        // Find the largest fully connected set
+        var largestset = connectedsets.MaxBy(s => s.Count);
+        if (largestset != null) {
+            Console.WriteLine(string.Join(',',largestset.ToList().Order()));
+        }
     }
 }

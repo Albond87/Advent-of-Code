8989cdb33a1db95ae61507faa653ba6f1baa9741
Albond87 <Albond8746@gmail.com>
2025-12-03 11:01:50 +0000

    2025 day 3

--- 2025/Puzzle03.cs
+++ 2025/Puzzle03.cs
@@ -1,14 +1,35 @@
 public class Puzzle03 : Puzzle
 {
-    public Puzzle03() : base("03") { }
+    readonly List<int>[] banks;
+
+    public Puzzle03() : base("03")
+    {
+        banks = inputs.Select(i => i.ToList().Select(c=>(int)char.GetNumericValue(c)).ToList()).ToArray();
+    }
+
+    static long GetLargestJoltage(List<int> bank, int numDigits)
+    {
+        long joltage = 0;
+        int startIndex = 0;
+        for (int d=numDigits-1; d>=0; d--)
+        {
+            joltage *= 10;
+            int digit = bank[startIndex..(bank.Count-d)].Max();
+            joltage += digit;
+            startIndex = bank.IndexOf(digit, startIndex) + 1;
+        }
+        return joltage;
+    }
 
     public override void Part1()
-    {        
-        Console.WriteLine();
+    {
+        long sum = banks.Select(b => GetLargestJoltage(b, 2)).Sum();
+        Console.WriteLine(sum);
     }
 
     public override void Part2()
     {
-        Console.WriteLine();
+        long sum = banks.Select(b => GetLargestJoltage(b, 12)).Sum();
+        Console.WriteLine(sum);
     }
 }
